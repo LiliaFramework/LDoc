bc65c9212073a10033f53244a1b7604d77f2ecb8
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-06-06 05:54:05 +0100

    x

--- ldoc.lua
+++ ldoc.lua
@@ -624,8 +624,8 @@ ldoc.readme = ldoc.readme or ldoc.topics
 if type(ldoc.readme) == 'string' then ldoc.readme = {ldoc.readme} end
 if type(ldoc.readme) == 'table' then
    process_file_list(ldoc.readme, '*.md', function(f)
+      print("PATH: "..f)
       local value = (f:find("^docs/store/.+%.md$") and 'store') or (f:find("^docs/structures/.+%.md$") and 'structures') or 'topic'
-      print(value)
       local item, F = add_special_project_entity(f, {
          class = value
       }, markup.add_sections, ldoc.pretty_topic_names)
